0f4b19333790dad2ba1f61d3736acd1ceeacc575
j0nl1 <jonlitech@gmail.com>
2023-02-10 10:10:33 +0100

    fix: font family in landing

--- apps/website/components/Landing/Overlay.tsx
+++ apps/website/components/Landing/Overlay.tsx
@@ -10,12 +10,12 @@ export const Overlay: React.FC<Props> = ({ fill }) => {
   return (
     <div className="relative order-2 h-[50vh] mb-[-20rem] md:mb-0 md:h-full md:order-1 flex-1 pointer-none">
       <a.svg viewBox="0 0 583 720" fill={fill} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-        <text fill="#566fa1" style={{ whiteSpace: 'pre' }} fontFamily="Anek Latin" fontSize={52} fontWeight="bold" letterSpacing="0em">
+        <text fill="#566fa1" style={{ whiteSpace: 'pre', fontWeight: '800', fontSize: 52 }} letterSpacing="0em">
           <tspan x={15} y={257.909}>
             {'Vectis Accounts \u2014'}
           </tspan>
         </text>
-        <text style={{ whiteSpace: 'pre' }} fontFamily="Anek Latin" fontSize={46} fontWeight="bold" letterSpacing="0em">
+        <text style={{ whiteSpace: 'pre', fontWeight: '800', fontSize: 46 }} letterSpacing="0em">
           <tspan x={15} y={321.909}>
             An interchain wallet
           </tspan>
