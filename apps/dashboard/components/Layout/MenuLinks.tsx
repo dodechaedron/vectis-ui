5419af12fc0ce793e8790e5666d8ac468e9c1fe0
j0nl1 <jonlitech@gmail.com>
2023-02-14 11:52:49 +0100

    feat: disable plugin link

--- apps/dashboard/components/Layout/MenuLinks.tsx
+++ apps/dashboard/components/Layout/MenuLinks.tsx
@@ -5,8 +5,6 @@ import { useVectis } from 'providers';
 import { generalMenu, toolsMenu } from 'utils/links';
 import Link from 'next/link';
 
-import { FiExternalLink } from 'react-icons/fi';
-
 interface Props {
   closeMenu: () => void;
 }
@@ -31,6 +29,7 @@ const MenuLinks: React.FC<Props> = ({ closeMenu }) => {
         {links.map((item) => {
           const path = pathname.slice(1);
           const isActive = item.href === pathname || path.includes(item.href);
+          const isDisabled = ["Governance", "Plugins"].includes(item.text);
           return (
             <li key={item.text}>
               <Link
@@ -38,18 +37,21 @@ const MenuLinks: React.FC<Props> = ({ closeMenu }) => {
                 onClick={closeMenu}
                 className={clsx(
                   isActive ? 'bg-gray-100 text-kashmir-blue-500' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                  'group flex w-full items-center rounded-md px-3 py-2 text-sm font-medium'
+                  'group flex w-full items-center rounded-md px-3 py-2 text-sm font-medium', {
+                  'hover:cursor-not-allowed hover:bg-transparent hover:text-gray-600': isDisabled
+                }
                 )}
               >
                 <item.Icon
                   className={clsx(
                     isActive ? 'text-kashmir-blue-500' : 'text-gray-400 group-hover:text-kashmir-blue-500',
-                    '-ml-1 mr-3 h-6 w-6 flex-shrink-0'
+                    '-ml-1 mr-3 h-6 w-6 flex-shrink-0',
+                    { 'text-gray-400 group-hover:text-gray-400': isDisabled }
                   )}
                   aria-hidden="true"
                 />
                 <span className="truncate">{item.text}</span>
-                {item.href.includes('plugins') && (
+                {item.text === "Plugins" && (
                   <span className="ml-2 inline-flex items-center rounded-full bg-kashmir-blue-100 px-2.5 py-0.5 text-xs font-medium text-kashmir-blue-800">
                     Soon
                   </span>
@@ -58,18 +60,6 @@ const MenuLinks: React.FC<Props> = ({ closeMenu }) => {
             </li>
           );
         })}
-        <li>
-          <a
-            className="group flex w-full items-center rounded-md px-3 py-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
-            target="_blank"
-            href="https://dao.vectis.xyz"
-            onClick={closeMenu}
-            rel="noreferrer"
-          >
-            <FiExternalLink className="-ml-1 mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-kashmir-blue-500" aria-hidden="true" />
-            <span className="truncate">Governance</span>
-          </a>
-        </li>
       </ul>
       <span className="my-5 h-[1px] w-full bg-gray-300" />
       <p className="mb-4 text-xs font-medium text-gray-400">Tools</p>
--- apps/dashboard/utils/links.ts
+++ apps/dashboard/utils/links.ts
@@ -1,5 +1,6 @@
 import { CubeTransparentIcon, PuzzlePieceIcon } from '@heroicons/react/24/outline';
 import { BiTransfer } from 'react-icons/bi';
+import { FiExternalLink } from 'react-icons/fi';
 import { ImGithub } from 'react-icons/im';
 import { IoMdHelpCircle } from 'react-icons/io';
 import { RiSecurePaymentFill } from 'react-icons/ri';
@@ -23,7 +24,8 @@ export const generalMenu = [
   { text: 'Guardian View', href: '/guardians', Icon: RiSecurePaymentFill },
   /*  { text: "Address Book", href: "/agenda", Icon: FaRegAddressBook }, */
   { text: 'Transactions', href: '/transactions', Icon: BiTransfer },
-  { text: 'Plugins', href: '/plugins', Icon: PuzzlePieceIcon }
+  { text: 'Plugins', href: '#', Icon: PuzzlePieceIcon },
+  { text: 'Governance', href: '#', Icon: FiExternalLink }
 ];
 
 export const toolsMenu = [{ text: 'Help & Support', href: 'https://discord.gg/xp3vFSAMgS', Icon: IoMdHelpCircle }];
