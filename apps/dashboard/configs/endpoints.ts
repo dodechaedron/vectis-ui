f2d21d933b1441857f13221032b4a98c5dd94aa7
j0nl1 <jonlitech@gmail.com>
2023-05-19 13:28:04 +0200

    chore: endpoints for archway and neutron testnet

--- apps/dashboard/configs/endpoints.ts
+++ apps/dashboard/configs/endpoints.ts
@@ -1,16 +1,29 @@
 import { Endpoints } from '~/interfaces';
 
-const injective_testnet = {
-  indexer: 'https://k8s.testnet.exchange.grpc-web.injective.network',
-  grpcUrl: 'https://k8s.testnet.chain.grpc-web.injective.network',
-  rpcUrl: 'https://k8s.testnet.tm.injective.network',
-  restUrl: 'https://k8s.testnet.lcd.injective.network',
-  chronos: 'https://k8s.testnet.exchange.grpc-web.injective.network',
-  explorer: 'https://k8s.testnet.explorer.grpc-web.injective.network'
+const custom_endpoints = {
+  injectivetestnet: {
+    indexer: 'https://k8s.testnet.exchange.grpc-web.injective.network',
+    grpcUrl: 'https://k8s.testnet.chain.grpc-web.injective.network',
+    rpcUrl: 'https://k8s.testnet.tm.injective.network',
+    restUrl: 'https://k8s.testnet.lcd.injective.network',
+    chronos: 'https://k8s.testnet.exchange.grpc-web.injective.network',
+    explorer: 'https://k8s.testnet.explorer.grpc-web.injective.network'
+  },
+  archwaytestnet: {
+    rpcUrl: 'https://rpc.constantine.archway.tech',
+    restUrl: 'https://api.constantine.archway.tech',
+    grpUrl: 'grpc.constantine.archway.tech:443'
+  },
+  neutrontestnet: {
+    rpcUrl: 'https://rpc-palvus.pion-1.ntrn.tech',
+    restUrl: 'https://rest-palvus.pion-1.ntrn.tech',
+    grpUrl: 'grpc-palvus.pion-1.ntrn.tech:80'
+  }
 };
 
 export const getEndpoints = (chainName: string): Endpoints => {
-  if (chainName === 'injectivetestnet') return injective_testnet;
+  const customEndpoints = custom_endpoints[chainName];
+  if (customEndpoints) return customEndpoints;
 
   const domain = chainName.includes('testnet') ? 'testcosmos.directory' : 'cosmos.directory';
   const explorerDomain = chainName.includes('testnet')
