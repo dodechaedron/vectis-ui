823fbd4ef889c7075830880423863efccae55d42
j0nl1 <jonlitech@gmail.com>
2023-05-19 17:44:56 +0200

    feat: support addresses from neutron and archway

--- apps/dashboard/configs/contracts.ts
+++ apps/dashboard/configs/contracts.ts
@@ -1,10 +1,14 @@
 // FACTORIES
 const junotestnet_factory = process.env.NEXT_PUBLIC_JUNO_FACTORY_ADDRESS;
 const injectivetestnet_factory = process.env.NEXT_PUBLIC_INJECTIVE_FACTORY_ADDRESS;
+const neutrontestnet_factory = process.env.NEXT_PUBLIC_NEUTRON_FACTORY_ADDRESS;
+const archwaytestnet_factory = process.env.NEXT_PUBLIC_ARCHWAY_FACTORY_ADDRESS;
 
 // PLUGIN REGISTRY
 const junotestnet_plugin_registry = process.env.NEXT_PUBLIC_JUNO_PLUGIN_REGISTRY_ADDRESS;
 const injectivetestnet_plugin_registry = process.env.NEXT_PUBLIC_INJECTIVE_PLUGIN_REGISTRY_ADDRESS;
+const neutrontestnet_plugin_registry = process.env.NEXT_PUBLIC_NEUTRON_PLUGIN_REGISTRY_ADDRESS;
+const archwaytestnet_plugin_registry = process.env.NEXT_PUBLIC_ARCHWAY_PLUGIN_REGISTRY_ADDRESS;
 
 const addresses = {
   junotestnet: {
@@ -14,6 +18,14 @@ const addresses = {
   injectivetestnet: {
     factoryAddress: injectivetestnet_factory,
     pluginRegistryAddress: injectivetestnet_plugin_registry
+  },
+  neutrontestnet: {
+    factoryAddress: neutrontestnet_factory,
+    pluginRegistryAddress: neutrontestnet_plugin_registry
+  },
+  archwaytestnet: {
+    factoryAddress: archwaytestnet_factory,
+    pluginRegistryAddress: archwaytestnet_plugin_registry
   }
 };
 
